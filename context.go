cdfccfe2276f4353fa53084213c9717583c03c30
Samuel <sam@infinitely.io>
2018-02-25 01:39:25 -0700

    Parse context utility

--- context.go
+++ context.go
@@ -10,3 +10,11 @@ import (
 func Context(c context.Context) router.Context {
 	return c.Value(router.RequestContext).(router.Context)
 }
+
+// Parse reads router context from context.Context
+func Parse(c context.Context) (router.Context, bool) {
+	if o, ok := c.Value(router.RequestContext).(router.Context); ok {
+		return o, true
+	}
+	return nil, false
+}
