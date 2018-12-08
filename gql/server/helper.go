2d83c267d3748123fb32bd07f0183c6666dfb7d8
Sam <sng@vaniila.com>
2018-12-07 20:39:46 -0700

    Fixes import cycle conflict

--- gql/server/helper.go
+++ gql/server/helper.go
@@ -3,11 +3,11 @@ package server
 import (
 	"context"
 
-	"github.com/vaniila/hyper/gql/graphql"
+	"github.com/vaniila/hyper/gql"
 	"github.com/vaniila/hyper/router"
 )
 
 // FromContext reads router context from context.Context
-func FromContext(c context.Context) graphql.Context {
+func FromContext(c context.Context) gql.Context {
 	return &ctx{c.Value(router.RequestContext).(router.Context)}
 }
